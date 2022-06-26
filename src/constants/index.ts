d2f97da43c9e2b1f478ca93ee68b09cc0b17d02e
dgesteves <diogo.esteves.goncalves@gmail.com>
2022-06-27 00:22:05 +0100

    added constants

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+import { INode } from "../services/types";
+
+export const BASE_URL = "data.json";
+export const PIPELINE_DESIGNER = "pipeline designer";
+export const DESIGNER_OPTIONS: INode[] = [
+  {
+    id: "c417cfa5-beb3-4b9b-92d9-d3d691296cf4",
+    name: "retriever",
+  },
+  {
+    id: "a9952d0a-2a46-4b2e-adde-8f0a0a63a3da",
+    name: "reader",
+  },
+];
